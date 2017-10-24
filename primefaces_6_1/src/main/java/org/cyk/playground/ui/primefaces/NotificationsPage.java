14c76d2dc177550ac69409d4cec48792ab73aecd
Christian Yao Komenan <Christian@CYK-HP-LAPTOP>
2017-10-24 15:57:47 +0000

    Change-Id: If0b7d32be33ffd4213e35024fe23dea728d8967b

--- primefaces_6_1/src/main/java/org/cyk/playground/ui/primefaces/NotificationsPage.java
+++ primefaces_6_1/src/main/java/org/cyk/playground/ui/primefaces/NotificationsPage.java
@@ -2,10 +2,12 @@ package org.cyk.playground.ui.primefaces;
 
 import java.io.Serializable;
 
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 import org.cyk.ui.web.primefaces.resources.page.Page;
+import org.cyk.utility.common.helper.CollectionHelper;
 import org.cyk.utility.common.helper.NotificationHelper;
 import org.cyk.utility.common.helper.NotificationHelper.SeverityType;
 
@@ -21,12 +23,23 @@ public class NotificationsPage extends Page implements Serializable {
 	protected void initialisation() {
 		super.initialisation();
 		getPropertiesMap().setTitle("Notifications");
+		
+		notificationsDialog.getPropertiesMap().setIdentifier("id123456");
+		notificationsDialog.getPropertiesMap().setHeader("HEADER");
+		notificationsDialog.getPropertiesMap().setModal(Boolean.TRUE);
+		notificationsDialog.getPropertiesMap().setAppendTo("@(body)");
+		//notificationsDialog.getPropertiesMap().setVisible("#{not empty facesContext.messageList}");
+		notificationsDialog.getPropertiesMap().setDraggable(Boolean.TRUE);
+		notificationsDialog.getPropertiesMap().setResizable(Boolean.TRUE);
+		notificationsDialog.getPropertiesMap().setClosable(Boolean.TRUE);
+		notificationsDialog.getPropertiesMap().setCloseOnEscape(Boolean.TRUE);
 	}
 	
 	public void oneMessageError(){
 		NotificationHelper.Notification notification = new NotificationHelper.Notification();
 		notification.setDetails("MyDetails").setSummary("MySummary").setSeverityType(SeverityType.ERROR);
-		NotificationHelper.getInstance().getViewer().setInput(notification).setType(NotificationHelper.Notification.Viewer.Type.DEFAULT).execute();
+		NotificationHelper.getInstance().getViewer().setInput(notification).setType(NotificationHelper.Notification.Viewer.Type.INLINE).execute();
+		notificationsDialog.getPropertiesMap().setVisible(CollectionHelper.getInstance().getSize(FacesContext.getCurrentInstance().getMessageList()) > 0);
 	}
 	
 	public void manyMessagesError(){
