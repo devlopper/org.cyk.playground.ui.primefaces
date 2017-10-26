c344a3759de140a3b18c6c58f5dffb2481f37e9d
Christian Yao Komenan <Christian@CYK-HP-LAPTOP>
2017-10-26 09:20:04 +0000

    Change-Id: Id13c688b87bf8f6565436991d01aedd328d0d694

--- primefaces_6_1/src/main/java/org/cyk/playground/ui/primefaces/FormsPage.java
+++ primefaces_6_1/src/main/java/org/cyk/playground/ui/primefaces/FormsPage.java
@@ -1,80 +1,50 @@
 package org.cyk.playground.ui.primefaces;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.cyk.ui.web.primefaces.resources.page.Page;
-import org.cyk.utility.common.helper.CommandHelper;
-import org.cyk.utility.common.userinterface.Layout;
-import org.cyk.utility.common.userinterface.command.Menu;
 import org.cyk.utility.common.userinterface.container.Form;
-import org.cyk.utility.common.userinterface.input.Input;
+import org.cyk.utility.common.userinterface.event.Confirm;
 import org.cyk.utility.common.userinterface.input.InputText;
 import org.primefaces.extensions.model.dynaform.DynaFormModel;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
 @Named @ViewScoped @Getter @Setter
 public class FormsPage extends Page implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Form.Master form;
-	private Form.Detail formDetail;
+	private DataModel dataModel = new DataModel();
 	private DynaFormModel model;  
 	
 	@Override
 	protected void initialisation() {
 		super.initialisation();
 		getPropertiesMap().setTitle("Forms");
-		form = new Form.Master();
+		form = new Form.Master(SubmitCommandActionAdapter.class).setObject(dataModel);
 		form.setLabelFromIdentifier("myformlabel");
+		form.getSubmitCommand().setConfirm(new Confirm());
 		
-		/*
-		formDetail = new Form.Detail();
-		formDetail.getLayout().setType(Layout.Type.VERTICAL);
-		
-		InputText c1 = new InputText();
-		c1.setLabelFromIdentifier("c1");
-		InputText c2 = new InputText();
-		c2.setLabelFromIdentifier("c2");
-		InputText c3 = new InputText();
-		c3.setLabelFromIdentifier("c3");
-		InputText c4 = new InputText();
-		c4.setLabelFromIdentifier("c4");
-		
-		formDetail.add(c1,c2,c3,c4);
-		form.setDetail(formDetail);
-		*/
-		form.setMenu(new Menu());
 		model = createModel();
-		form.setDetail(formDetail);
-		formDetail.getPropertiesMap().setValue(model);
-		
-		form.getSubmitCommand().setAction(new CommandHelper.Command.Adapter.Default(){
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			protected Object __execute__() {
-				for(Object object : formDetail.getLayout().getChildren().getElements())
-					System.out.println( ((Input<?>)object).getLabel().getPropertiesMap().getValue()+" = "+((Input<?>)object).getValue() );
-				return null;
-			}
-		});
-		
-		form.getSubmitCommand().getAction().setIsNotifiableOnStatusSuccess(Boolean.TRUE);
-		//form.getSubmitCommand().setConfirm(new Confirm());
-		
 	}
 	
 	private DynaFormModel createModel(){
-    	formDetail = new Form.Detail();
+    	Form.Detail formDetail = new Form.Detail();
+    	form.setDetail(formDetail);
+    	
     	InputText c1 = new InputText();
-    	c1.setLabelFromIdentifier("f1");
+    	c1.setLabelFromIdentifier("f1").setField(dataModel, "firstName");
     	InputText c2 = new InputText();
-    	c2.setLabelFromIdentifier("f2");
+    	c2.setLabelFromIdentifier("f2").setField(dataModel, "lastName");
     	InputText c3 = new InputText();
     	c3.setLabelFromIdentifier("f3");
     	InputText c4 = new InputText();
@@ -84,7 +54,7 @@ public class FormsPage extends Page implements Serializable {
     	InputText c6 = new InputText();
     	c6.setLabelFromIdentifier("f6");
     	InputText c7 = new InputText();
-    	c7.setLabelFromIdentifier("f7");
+    	c7.setLabelFromIdentifier("f7").setField(dataModel, "otherDetails");
     	c7.getArea().getWidth().setDistance(2);
     	InputText c8 = new InputText();
     	c8.setLabelFromIdentifier("f8");
@@ -93,8 +63,35 @@ public class FormsPage extends Page implements Serializable {
     	formDetail.layOut(c1).layOut(c2).layOutBreak().layOut(c3).layOutBreak().layOut(c4).layOut(c5).layOutBreak().layOut(c6).layOut(c7).layOutBreak().layOut(c8).layOutBreak();
 		
     	DynaFormModel model = (DynaFormModel) Form.Detail.buildTarget(formDetail);
-    	
+		formDetail.getPropertiesMap().setValue(model);
+		
         return model;
     }
 	
+	@Getter @Setter
+	public static class DataModel {
+		
+		private String firstName;
+		private String lastName;
+		private Date dateOfBirth;
+		private String otherDetails;
+		
+		@Override
+		public String toString() {
+			return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+		}
+	}
+	
+	@Getter @Setter @Accessors(chain=true)
+	public static class SubmitCommandActionAdapter extends org.cyk.utility.common.userinterface.container.Form.Master.SubmitCommandActionAdapter implements Serializable{
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		protected Object __execute__() {
+			super.__execute__();
+			System.out.println("FormsPage.SubmitCommandActionAdapter.__execute__() : CALL BUSINESS SERVICE to handle data : "+form.getObject());
+			return null;
+		}
+		
+	}
 }
