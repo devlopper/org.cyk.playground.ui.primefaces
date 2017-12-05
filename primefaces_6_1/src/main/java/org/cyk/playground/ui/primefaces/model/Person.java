5edaa50f85cb3a4d64d418016431dfb17bdebabd
Christian Yao Komenan <christian.komenan@dgbf.gouv.ci>
2017-12-05 15:54:24 +0000

    Change-Id: I59f315666f665c474f896a10dada3580929d746b

--- primefaces_6_1/src/main/java/org/cyk/playground/ui/primefaces/model/Person.java
+++ primefaces_6_1/src/main/java/org/cyk/playground/ui/primefaces/model/Person.java
@@ -1,12 +1,14 @@
 package org.cyk.playground.ui.primefaces.model;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.cyk.playground.ui.primefaces.ContextListener;
 import org.cyk.utility.common.helper.FileHelper;
+import org.cyk.utility.common.helper.RandomHelper;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -41,11 +43,33 @@ public class Person extends AbstractIdentified {
 		person.getGlobalIdentifier().setImage(new File(FileHelper.getInstance().get(ContextListener.class, image)));
 		COLLECTION.add(person);
 	}
+	
+	public static Person instanciateOneRandomly(){
+		RandomHelper.Person randomPerson = RandomHelper.getInstance().getPerson();
+		Person person = new Person();
+		person.setGlobalIdentifier(new GlobalIdentifier());
+		person.getGlobalIdentifier().setCode(RandomHelper.getInstance().getAlphabetic(5));
+		person.getGlobalIdentifier().setName(randomPerson.getFirstname());
+		person.setLastnames(randomPerson.getLastname());
+		person.getGlobalIdentifier().setImage(new File(randomPerson.getHeadOnlyPhoto()));
+		return person;
+	}
+	
+	public static Collection<Person> instanciateManyRandomly(Integer count){
+		Collection<Person> persons = new ArrayList<>();
+		for(Integer index = 0 ; index < count ; index ++)
+			persons.add(instanciateOneRandomly());
+		return persons;
+	}
 
-	public static Person get(String code) {
-		for(Person person : COLLECTION)
+	public static Person get(String code,Collection<Person> persons) {
+		for(Person person : persons)
 			if(person.getCode().equals(code))
 				return person;
 		return null;
+	}
+	
+	public static Person get(String code) {
+		return get(code,COLLECTION);
 	}
 }
