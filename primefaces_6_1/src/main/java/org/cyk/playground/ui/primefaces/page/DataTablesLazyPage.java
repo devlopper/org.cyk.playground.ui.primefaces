facf0f727eaee6c230c272cd67d548a5e1e97830
Christian Yao Komenan <Christian@CYK-HP-LAPTOP>
2017-12-08 08:09:27 +0000

    Change-Id: I28d721f58da18ccea9f568d961801b1a8b0e25fc

--- primefaces_6_1/src/main/java/org/cyk/playground/ui/primefaces/page/DataTablesLazyPage.java
+++ primefaces_6_1/src/main/java/org/cyk/playground/ui/primefaces/page/DataTablesLazyPage.java
@@ -1,15 +1,18 @@
 package org.cyk.playground.ui.primefaces.page;
 
 import java.io.Serializable;
-import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
+import org.cyk.playground.ui.primefaces.model.AbstractIdentified;
 import org.cyk.playground.ui.primefaces.model.Person;
 import org.cyk.utility.common.userinterface.Component;
 import org.cyk.utility.common.userinterface.collection.DataTable;
 import org.cyk.utility.common.userinterface.container.window.Window;
+import org.primefaces.model.SortOrder;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -18,42 +21,53 @@ import lombok.Setter;
 public class DataTablesLazyPage extends Window implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	private DataTable personDataTable1,personDataTable2,personDataTable3,personDataTable4;
+	private static final List<Person> PERSONS = (List<Person>) Person.instanciateManyRandomly(200);
+	
+	private DataTable personDataTable;
+	private Integer page = 5;
 	
 	@Override
 	protected void initialisation() {
 		super.initialisation();
-		getPropertiesMap().setTitle("Data tables Load");
+		getPropertiesMap().setTitle("Data tables Lazy");
 		
-		personDataTable1 = createPersonDataTable(null,null,null,Boolean.FALSE, Person.instanciateManyRandomly(5));
-		personDataTable2 = createPersonDataTable(null,null,null,Boolean.TRUE, Person.instanciateManyRandomly(5));
+		personDataTable = createDataTable(Person.class,new String[]{"globalIdentifier.code","globalIdentifier.name","lastnames"},page);
 		
-		personDataTable3 = createPersonDataTable(Boolean.TRUE,Boolean.TRUE,Boolean.TRUE,Boolean.FALSE, Person.instanciateManyRandomly(5));
-		personDataTable4 = createPersonDataTable(Boolean.TRUE,Boolean.TRUE,Boolean.TRUE,Boolean.TRUE, Person.instanciateManyRandomly(5));
 	}
 	
-	private DataTable createPersonDataTable(Boolean onPrepareAddMenu,Boolean onPrepareAddColumnOrderNumber,Boolean onPrepareAddColumnAction,Boolean onPrepareCallLoad
-			,final Collection<Person> persons){
-		DataTable dataTable = new DataTable(){
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public Component load() {
-				addManyRow(persons);
-				return this;
-			}
-		};
-		dataTable.setOnPrepareAddMenu(onPrepareAddMenu);
-		dataTable.setOnPrepareAddColumnAction(onPrepareAddColumnAction);
-		dataTable.setOnPrepareAddColumnOrderNumber(onPrepareAddColumnOrderNumber);
-		dataTable.setActionOnClass(Person.class);
-		dataTable.addColumnsByFieldNames("globalIdentifier.code","globalIdentifier.name","lastnames");
-		dataTable.setOnPrepareCallLoad(onPrepareCallLoad);
+	private <T extends AbstractIdentified> DataTable createDataTable(Class<T> aClass,String[] fieldNames,Integer page){
+		DataTable dataTable = new DataTable();
+		
+		dataTable.setActionOnClass(aClass);
+		dataTable.addColumnsByFieldNames(fieldNames);
+		dataTable.getPropertiesMap().setRows(page);
+		dataTable.getPropertiesMap().setPaginator(page != null);
+		dataTable.getPropertiesMap().setLazy(Boolean.TRUE);
 		dataTable.prepare();
 		dataTable.build();
-		if(!Boolean.TRUE.equals(onPrepareCallLoad))
-			dataTable.load();
+		
+		dataTable.getPropertiesMap().setValue(new LazyDataModel<T>(dataTable));
 		return dataTable;
 	}
 	
+	public static class LazyDataModel<T extends AbstractIdentified> extends org.primefaces.model.LazyDataModel<DataTable.Row> implements Serializable {
+		private static final long serialVersionUID = 1L;
+		
+		private Component component;
+		
+		public LazyDataModel(Component component) {
+			this.component = component;
+		}
+		
+		@Override
+		public List<DataTable.Row> load(int first, int pageSize, String sortField,SortOrder sortOrder, Map<String, Object> filters) {
+			@SuppressWarnings("unchecked")
+			List<T> p = (List<T>) PERSONS.subList(first, first + pageSize > PERSONS.size() ? PERSONS.size() -1 : first + pageSize);
+			setRowCount(PERSONS.size());
+			return (List<DataTable.Row>) DataTable.Row.instanciateMany(p,component,null);
+		}
+		
+		
+		
+	}
 }
